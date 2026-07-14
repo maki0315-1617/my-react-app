7584d149ee6521e22c8e6d11662fa337582d99df
maki0315-1617 <ronron201907@gmail.com>
2026-07-14 16:06:43 +0900

    スペルミス

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [ch1ount, setCount] = useState(0)
+  const [count, setCount] = useState(0)
 
   return (
     <>
